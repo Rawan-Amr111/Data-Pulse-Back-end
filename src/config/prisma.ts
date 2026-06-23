b4c29bb10968613ed97a1625b142c424a32d7b4e
Ahmed Yehia <ahmedyehia2715@gmail.com>
2026-06-23 18:25:47 +0300

    Resolve prisma schema conflict

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-export const prisma = new PrismaClient();
+// 1. Initialize the native database driver client
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+// 2. Wrap it with the Prisma Driver Adapter
+const adapter = new PrismaPg(pool);
+
+// 3. Instantiate the Prisma Client with the adapter
+export const prisma = new PrismaClient({ adapter });
